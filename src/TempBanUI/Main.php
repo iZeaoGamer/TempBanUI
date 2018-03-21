3075621e0a7903b8f7eb13c609d8ec0bcd0de95c
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-03-21 16:53:13 +0000

    Update Main.php

--- src/TempBanUI/Main.php
+++ src/TempBanUI/Main.php
@@ -98,7 +98,7 @@ class Main extends PluginBase implements Listener {
 						}
 						$form->setTitle(TextFormat::BOLD . "§6Vois§bFactions§cPE §dTemp Ban System");
 						$form->addDropdown("\n§aChoose a player to ban", $list);
-						$form->addInput("Time");
+						$form->addInput("Time". $banTime);
 						$form->addInput("Reason");
 						$form->sendToPlayer($sender);
 					}
